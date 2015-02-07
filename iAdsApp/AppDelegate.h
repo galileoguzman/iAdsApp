61358a773e5d0d1d86d2dd51085f706796735e99
Galileo Guzmán <galileoguzman@gmail.com>
2015-02-07 09:55:23 -0600

    Initial Commit

---
+++ iAdsApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iAdsApp
+//
+//  Created by Galileo Guzman on 07/02/15.
+//  Copyright (c) 2015 Galileo Guzman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iAdsApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iAdsApp
+//
+//  Created by Galileo Guzman on 07/02/15.
+//  Copyright (c) 2015 Galileo Guzman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
